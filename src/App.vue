c5662e26f1ad0c31e3dc5cd19566d5d7dc790a9a
YohanStoffers <91470337+YohanStoffers@users.noreply.github.com>
2023-03-13 14:37:02 +0100

    changed template layout

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,24 @@
     <header>
         <div class="startClass">
             <nav class="p-2">
-                <RouterLink to="/">Home</RouterLink>
-                <RouterLink to="/about">About</RouterLink>
+                <h3 class="m-2">nav</h3>
+                <RouterLink class="m-2" to="/">Home</RouterLink>
+                <RouterLink class="m-2" to="/about">About</RouterLink>
             </nav>
         </div>
     </header>
     <div>
+        <div class="m-4">
+            <strong>From API template:</strong>
+            {{ getTemplateData.body }}
+        </div>
         <RouterView />
     </div>
 </template>
 
 <script setup lang="ts">
 import {RouterLink, RouterView} from 'vue-router';
+import {getTemplateData} from './api';
 </script>
 <style scoped>
 .startClass {
